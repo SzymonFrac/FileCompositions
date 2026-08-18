22c12625082ad3edc6bad50080714f389cc6f7ce
SzymonFrac <szymon.frac195@gmail.com>
2026-08-19 00:12:13 +0100

    NoFileDefinition will not be implemented (see #15)

--- src/FileCompositions.Core/File/No/Definition/INoFileDefinition.cs
+++
@@ -1,9 +0,0 @@
-﻿using FileCompositions.Core.File.Definition;
-using FileCompositions.Core.Quality.Ownership;
-using FileCompositions.Core.Quality.Placement;
-
-namespace FileCompositions.Core.File.No.Definition;
-
-public interface INoFileDefinition<TOwnership, TPlacement> : IFileDefinition<TOwnership, TPlacement>
-    where TOwnership : DefinitionOwnership
-    where TPlacement : DefinitionPlacement;
